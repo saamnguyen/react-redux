a2e4b18fb1286e9a0217e7d3e359773ebedd1019
finn-01 <nguyensam576@gmail.com>
2022-03-25 00:21:29 +0700

    actions

---
+++ src/redux/actions.js
@@ -0,0 +1,12 @@
+// export const addTodoAction = {
+//     type: 'todoList/addTodo',
+//     payload: { id: 1, name: "Learn ReactJS", completed: false, priority: "Medium" },
+// }
+
+//action creators
+export const addTodo = (data) => {
+	return {
+		type: "todoList/addTodo",
+		payload: data,
+	};
+};
