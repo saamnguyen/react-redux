46c0fb9c71dd37a0d76cb804c35f1ab8d6402e50
finn-01 <nguyensam576@gmail.com>
2022-03-30 01:28:49 +0700

    filter status1

--- src/components/Filters/FiltersSlice.js
+++ src/components/Filters/FiltersSlice.js
@@ -11,6 +11,11 @@ const filtersReducer = (state = initState, action) => {
 				...state,
 				search: action.payload,
 			};
+		case "filters/statusFilterChange":
+			return {
+				...state,
+				status: action.payload,
+			};
 		default:
 			return state;
 	}
